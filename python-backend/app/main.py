9cedfb30e8f412d0fa8215fdde718cebf9df1836
yogeshwarsaini <yogismash123@gmail.com>
2026-08-01 21:56:01 +0530

    fixing cors again

--- python-backend/app/main.py
+++ python-backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI(title="Fees Portal Backend", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://app-plateform.*\vercel\.app",
+    allow_origins=[
+        "https://app-plateform.vercel.app",
+    ],
+    allow_origin_regex=r"https://app-plateform.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
